ce7cc07ed587b688d7a5f10325dbf4cbd0006fb6
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-05 19:20:07 -0300

    chore: should sum return 4

---
+++ test/calc.spec.js
@@ -0,0 +1,11 @@
+var calcModule = require('../calc')
+var chai = require('chai')
+var expect = chai.expect
+
+describe('calc test', function(){
+  it('Should return 4', function(done){
+    var result = calcModule.sum(2,2)
+    expect(result).to.equal(4)
+    done()
+  })
+})
