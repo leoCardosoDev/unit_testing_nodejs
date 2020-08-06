d80782f598d900b20614f9eb06992e3771e15d19
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-06 10:46:36 -0300

    feat: Should sum return 4

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  sum: function(n1, n2){
+  sum: (n1, n2) => {
     return n1 + n2
   },
   sub: function(n1, n2){
--- test/calc.spec.js
+++ test/calc.spec.js
@@ -1,11 +1,29 @@
-var calcModule = require('../calc')
-var chai = require('chai')
-var expect = chai.expect
+const calcModule = require('../calc')
+const chai = require('chai')
+const expect = chai.expect
 
+import sinon from 'sinon'
+
+/** 
 describe('calc test', function(){
-  it('Should return 4', function(done){
-    var result = calcModule.sum(2,2)
+  it('Should sum return 4', (done) => {
+    let result = calcModule.sum(2,2)
     expect(result).to.equal(4)
     done()
+  })
+})
+*/
+
+describe('Sum Test', function(){
+  it('Should sum return 4', (done) => {
+    let mock = sinon.mock(calcModule)
+    mock.expects('sum').yields(null, 4)
+    calcModule.sum(2,2, (err, result) => {
+      mock.verify()
+      mock.restore()
+      
+      expect(result).to.be.equal(4)
+      done()
+    })
   })
 })
