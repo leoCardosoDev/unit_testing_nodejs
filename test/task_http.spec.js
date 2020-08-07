9491a49d22f1cecf316eba6ddbd7c799497dd3e0
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-07 16:53:22 -0300

    test: Should make a rquest an not send name form data

--- test/task_http.spec.js
+++ test/task_http.spec.js
@@ -13,15 +13,26 @@ const uri = 'http://localhost:3000'
 describe('Task Request', () => {
 
   it('Should make a request and return a list of task', (done) => {
-
     request(uri).get('/task').end((err, res) => {
-      if(!err){
+      if (!err) {
         expect(res.body.status).to.be.true
         expect(res.body.data).to.be.an('array')
         done()
       }
     })
+  })
 
+  it('Should make a request an not send name form data', (done) => {
+    request(uri)
+      .post('/task')
+      .send()
+      .end((err, res) => {
+        if (err) {
+          expect(res.body.status).to.be.false
+          expect(res).to.have.status(500)
+          done()
+        }
+      })
   })
 
 })
